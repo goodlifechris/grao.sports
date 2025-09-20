2b37a41a6da53f23779ca79a21c2eca21901cd5e
goodlifechris <christopherndugo@gmail.com>
2025-09-20 22:00:35 +0400

    testing upload of file

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -43,7 +43,7 @@ export default function PostEditor() {
         italic: false,
       }),
       Placeholder.configure({
-        placeholder: "What's crack-a-lackin'?",
+        placeholder: "#TukutaneGrao Share, what's happening around in sports  ?",
       }),
     ],
   });
