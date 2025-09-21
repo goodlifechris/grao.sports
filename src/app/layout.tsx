9d75a6b2f04a6a49c8aba4f22e5e887d7de6493d
goodlifechris <christopherndugo@gmail.com>
2025-09-21 17:32:02 +0400

    set up redis for feetching data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         {/* Google Analytics Script */}
         <Script
